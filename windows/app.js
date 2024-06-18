6176c3da7926408a599c5c61a771d995279903a9
terrablue <102580937+terrablue@users.noreply.github.com>
2024-06-18 22:51:51 +0200

    initial commit

---
+++ windows/app.js
@@ -0,0 +1,12 @@
+// import worker as asset
+import worker_asset from "../worker.js" with { type: "file" };
+// import a worker dependency as asset
+import libwebview from "./libwebview.dll" with { type: "file" };
+
+// start server
+Bun.serve({ port: 6161, fetch: () => new Response("hi") });
+
+// start worker
+const worker = new Worker(URL.createObjectURL(Bun.file(worker_asset)));
+// pass in dependency URL
+worker.postMessage({ dependencies: { libwebview } });
